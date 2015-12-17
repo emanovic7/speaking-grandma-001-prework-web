e3fdcf06ebc398986d108fc81913b62d9e46f98d
emanovic7 <e_owusu@live.com>
2015-12-17 14:59:42 +0000

    nitrous-backup at Thu Dec 17 14:59:42 UTC 2015

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,17 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+def speak_to_grandma(phrase)
+  happy_grandma = "NO, NOT SINCE 1938!"
+  unhappy_grandma = "HUH?! SPEAK UP, SONNY!"
+  if phrase == phrase.upcase
+    return happy_grandma
+  else
+    return unhappy_grandma
+  end
+end
